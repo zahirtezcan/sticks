76f2e55d1aded47ee3ec4ca07568a7bb55e3cd07
zahir <zahirtezcan@yahoo.com>
2015-11-05 19:44:54 +0200

    TestGenerate modified for gtest

--- test/algorithm/TestGenerate.cpp
+++ test/algorithm/TestGenerate.cpp
@@ -1,60 +1,52 @@
 #include <stx/algorithm/Generate.h>
 #include <vector>
-#include <iostream>
+#include <gtest/gtest.h>
 
 template<typename Container>
-bool ElementsEqual(const Container& c1, const Container& c2) {
-	if (c1.size() != c2.size()) return false;
+testing::AssertionResult ElementsEqual(const Container& c1, const Container& c2) {
+	if (c1.size() != c2.size()) {
+		return testing::AssertionFailure() << " container size mismatch ";
+	}
 
 	for (typename Container::size_type i = 0; i < c1.size(); ++i) {
-		if (c1[i] != c2[i]) return false;
+		if (c1[i] != c2[i]) {
+			return testing::AssertionFailure() << " mismatch at index " << i;
+		}
 	}
 
-	return true;
+	return testing::AssertionSuccess();
 }
 
-int TestGenerateBasic()
+TEST(Generate, Basic)
 {
-	std::cout << "Testing Generate...";
+	std::vector<int> expected(100, 13);
+	auto expectedIter = expected.begin();
 
-	std::vector<int> base, generated;
-	base.assign(100, 13);
-	generated.resize(base.size());
-	stx::Generate(generated.begin(), generated.end(), [&]() { return base[0]; });
+	std::vector<int> generated;
+	generated.resize(expected.size());
+	stx::Generate(generated.begin(), generated.end(), [&]() { return *expectedIter++; });
 
-	if (!ElementsEqual(base, generated)) {
-		std::cerr << "Generate basic failed!" << std::endl;
-		return -1;
-	}
-
-	std::cout << "OK\n";
-	return 0;
+	EXPECT_TRUE(ElementsEqual(expected, generated));
 }
 
-int TestGenerateIndexed()
+TEST(Generate, Increment)
 {
-	std::cout << "Testing GenerateIndexed...";
+	std::vector<int> expected = { 1, 2, 3 };
+	std::vector<int> generated(expected.size());
 
-	std::vector<int> base, generated;
-	base.resize(100);
-	stx::GenerateIncrement(base.begin(), base.end(), 13);
+	stx::GenerateIncrement(generated.begin(), generated.end(), 1);
 
-	generated.resize(base.size());
-	stx::GenerateIndexed(generated.begin(), generated.end(), [&](int i) { return base[i]; });
-
-	if (!ElementsEqual(base, generated)) {
-		std::cerr << "GenerateIndex failed!" << std::endl;
-		return -1;
-	}
-
-	std::cout << "OK\n";
-	return 0;
+	EXPECT_TRUE(ElementsEqual(expected, generated));
 }
 
-int main()
+TEST(Generate, Indexed)
 {
-	if (TestGenerateBasic() != 0) return -1;
-	if (TestGenerateIndexed() != 0) return -1;
+	std::vector<int> expected(100);
+	stx::GenerateIncrement(expected.begin(), expected.end(), 1);
 
-	return 0;
+	std::vector<int> generated(expected.size());
+	stx::GenerateIndexed(generated.begin(), generated.end(), [&](int i) { return expected[i]; });
+
+	EXPECT_TRUE(ElementsEqual(expected, generated));
 }
+
