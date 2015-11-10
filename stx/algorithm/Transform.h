1fb4d2e030dcf39d96688f724e183005b7e8f31c
zahir <zahirtezcan@yahoo.com>
2015-11-10 22:02:16 +0200

    implemented transform algorithm

---
+++ stx/algorithm/Transform.h
@@ -0,0 +1,49 @@
+#ifndef STX_ALGORITHM_TRANSFORM_H
+#define STX_ALGORITHM_TRANSFORM_H
+
+#include <stx/Iterator.h>
+
+namespace stx {
+
+template<typename InputIterator, typename OutputIterator, typename UnaryFunction>
+OutputIterator Transform(InputIterator begin, InputIterator end,
+                         OutputIterator output,
+                         UnaryFunction transform)
+{
+	while (begin != end) {
+		*output = transform(*begin);
+		++begin;
+		++output;
+	}
+	return output;
+}
+
+/* In STL there also is a transform with 2 input sequences, which are in turn transformed by a binary operation.
+ * That method does not take an end for second iterator sequence, I think it should.
+ * That method only returns iterated output iterator BUT not the second input iterator
+ *     I could have implemented a method with an ending secondary sequence
+ *     or return a pair of iterators both for second input sequence and output iterator
+ *        But implementing a proper zip-iterator will be better
+ *           proper: may be i can make sort possible with it
+ */
+
+/**/
+template<typename InputIterator, typename OutputIterator, typename UnaryIndexedFunction>
+OutputIterator TransformIndexed(InputIterator begin, InputIterator end,
+                                OutputIterator output,
+                                UnaryIndexedFunction transform)
+{
+	stx::IteratorDifference<InputIterator> index = 0;
+
+	while (begin != end) {
+		*output = transform(*begin, index);
+		++begin;
+		++index;
+		++output;
+	}
+	return output;
+}
+
+}
+
+#endif
---
+++ test/algorithm/TestTransform.cpp
@@ -0,0 +1,91 @@
+#include <stx/algorithm/Transform.h>
+#include <gtest/gtest.h>
+#include <vector>
+
+TEST(Transform, Empty)
+{
+	std::vector<int> v;
+	std::vector<int> output(100, 0);
+
+	auto result = stx::Transform(v.begin(), v.end(), output.begin(), [](int) { return -1; });
+
+	EXPECT_EQ(0, output[0]);
+	EXPECT_EQ(output.begin(), result);
+}
+
+TEST(Transform, Basic)
+{
+	std::vector<int> v = { 1, 2, 3 };
+	std::vector<int> output(100, 0);
+
+	auto result = stx::Transform(v.begin(), v.end(), output.begin(), [](int x) { return x; });
+
+	EXPECT_EQ(v[0], output[0]);
+	EXPECT_EQ(v[1], output[1]);
+	EXPECT_EQ(v[2], output[2]);
+	EXPECT_EQ(output.begin() + v.size(), result);
+}
+
+TEST(Transform, Cast)
+{
+	std::vector<int> v = { 1, 2, 3 };
+	std::vector<double> output(100, 0.0);
+
+	auto result = stx::Transform(v.begin(), v.end(), output.begin(), [](int x) { return static_cast<double>(x); });
+
+	EXPECT_EQ(v[0], output[0]);
+	EXPECT_EQ(v[1], output[1]);
+	EXPECT_EQ(v[2], output[2]);
+	EXPECT_EQ(output.begin() + v.size(), result);
+}
+
+TEST(TransformIndexed, Empty)
+{
+	std::vector<int> v;
+	std::vector<int> output(100, 0);
+
+	auto result = stx::TransformIndexed(v.begin(), v.end(), output.begin(), [](int, int) { return -1; });
+
+	EXPECT_EQ(0, output[0]);
+	EXPECT_EQ(output.begin(), result);
+}
+
+TEST(TransformIndexed, BasicNoIndex)
+{
+	std::vector<int> v = { 1, 2, 3 };
+	std::vector<int> output(100, 0);
+
+	auto result = stx::TransformIndexed(v.begin(), v.end(), output.begin(), [](int x, int) { return x; });
+
+	EXPECT_EQ(v[0], output[0]);
+	EXPECT_EQ(v[1], output[1]);
+	EXPECT_EQ(v[2], output[2]);
+	EXPECT_EQ(output.begin() + v.size(), result);
+}
+
+TEST(TransformIndexed, Basic)
+{
+	std::vector<int> v = { 1, 2, 3 };
+	std::vector<int> output(100, -1);
+
+	auto result = stx::TransformIndexed(v.begin(), v.end(), output.begin(), [](int, int index) { return index; });
+
+	EXPECT_EQ(0, output[0]);
+	EXPECT_EQ(1, output[1]);
+	EXPECT_EQ(2, output[2]);
+	EXPECT_EQ(output.begin() + v.size(), result);
+}
+
+TEST(TransformIndexed, Power)
+{
+	std::vector<int> v = { 1, 2, 3 };
+	std::vector<double> output(100, 0.0);
+
+	auto result = stx::TransformIndexed(v.begin(), v.end(), output.begin(), [](int x, int index) { return pow(x, index); });
+
+	EXPECT_EQ(1, output[0]);
+	EXPECT_EQ(2, output[1]);
+	EXPECT_EQ(9, output[2]);
+	EXPECT_EQ(output.begin() + v.size(), result);
+}
+
