d8c4cdde9c1dd574192654bf48f00426fb6c5585
zahir <zahirtezcan@yahoo.com>
2015-11-11 21:48:10 +0200

    implemented findrepitition algorithm

--- stx/algorithm/Find.h
+++ stx/algorithm/Find.h
@@ -130,6 +130,42 @@ Iterator FindLastSequence(Iterator begin, Iterator end,
 	return result;
 }
 
+template<typename Iterator, typename Size, typename T>
+Iterator FindRepitition(Iterator begin, Iterator end,
+                        Size count, const T& value)
+{
+	return FindRepitition(begin, end, count, value, stx::Equals());
+}
+
+template<typename Iterator, typename Size, typename T, typename BinaryPredicate>
+Iterator FindRepitition(Iterator begin, Iterator end,
+                        Size count, const T& value,
+			BinaryPredicate equals)
+{
+	while (begin != end) {
+		auto srcIter = begin;
+		
+		Size thisCount = 0;
+		while (true) {
+			if (thisCount == count) {
+				return begin;
+			} else if (srcIter == end) {
+				return end;
+			} else if (!equals(*srcIter, value)) {
+				begin = srcIter;
+				break;
+			}
+
+			++thisCount;
+			++srcIter;
+		}
+
+		++begin;
+	}
+
+	return end;
+}
+
 }/*end of stx namespace*/
 
 #endif
