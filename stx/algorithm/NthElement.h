c3bbe17e0431bf0225704f3d0bb95d186e368c5d
zahir <zahirtezcan@yahoo.com>
2015-12-06 12:38:29 +0200

    implemented nth-element in a lousy way (looks like quick-sort, need to look into intro-select)

--- stx/algorithm/NthElement.h
+++ stx/algorithm/NthElement.h
@@ -1,14 +1,57 @@
 #ifndef STX_ALGORITHM_NTHELEMENT_H
 #define STX_ALGORITHM_NTHELEMENT_H
 
+#include <stx/Iterator.h>
 #include <stx/utility/Compare.h>
+#include <stx/algorithm/SwapPointee.h>
+#include <stx/algorithm/Partition.h>
 
 namespace stx {
 
 template<typename Iterator, typename Compare>
-void NthElement(Iterator /*begin*/, Iterator /*nth*/, Iterator /*end*/,
-                Compare /*compare*/)
+void NthElement(Iterator begin, Iterator nth, Iterator end,
+                Compare compare)
 {
+	using std::distance;
+	using std::advance;
+	
+	if (begin == end) {
+		return;
+	}
+
+	auto n = distance(begin, nth);
+	auto pbegin = begin;
+	auto pend = end;
+
+	while (pbegin != pend) {
+		auto pdist = distance(pbegin, pend);
+		if (pdist == 1) {
+			break;
+		}
+
+		auto mid = pbegin;
+		advance(mid, pdist / 2);
+		stx::SwapPointee(pbegin, mid);
+
+		auto iter = pbegin;
+		++iter;
+		auto ppoint = stx::Partition(iter, pend, [&](auto& x) {
+			return compare(x, *pbegin);
+		});
+		--ppoint;
+		stx::SwapPointee(pbegin, ppoint);
+	
+		if (ppoint == nth) {
+			break;
+		}
+
+		auto ppdist = distance(begin, ppoint);
+		if (ppdist > n) {
+			pend = ppoint;
+		} else {
+			pbegin = ppoint;
+		}
+	}
 }
 
 template<typename Iterator>
--- test/algorithm/TestNthElement.cpp
+++ test/algorithm/TestNthElement.cpp
@@ -60,32 +60,20 @@ TEST(NthElement, TwoReverseSecond)
 	EXPECT_EQ(2, v[1]);
 }
 
-#include <iostream>
-
-template<typename C>
-void Print(const C& c) {
-	for (auto& x : c) {
-		std::cout << x << ", ";
-	}
-	std::cout << std::endl;
-}
-
 TEST(NthElement, Basic)
 {
 	std::vector<int> v = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
 	
-	Print(v);
 	stx::NthElement(v.begin(), v.begin() + 3, v.end());
-	Print(v);
 
 	EXPECT_EQ(4, v[3]);
 	EXPECT_LT(v[0], 4);
 	EXPECT_LT(v[1], 4);
 	EXPECT_LT(v[2], 4);
-	EXPECT_GT(4, v[4]);
-	EXPECT_GT(4, v[5]);
-	EXPECT_GT(4, v[6]);
-	EXPECT_GT(4, v[7]);
-	EXPECT_GT(4, v[8]);
+	EXPECT_LT(4, v[4]);
+	EXPECT_LT(4, v[5]);
+	EXPECT_LT(4, v[6]);
+	EXPECT_LT(4, v[7]);
+	EXPECT_LT(4, v[8]);
 }
 
