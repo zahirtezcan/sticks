cd63ad9d73588c6d532da0dda1702ceceaabcb20
zahir <zahirtezcan@yahoo.com>
2015-12-20 11:32:14 +0200

    modified Compare parameters to be copied by reference

--- stx/algorithm/BinarySearch.h
+++ stx/algorithm/BinarySearch.h
@@ -10,7 +10,7 @@ namespace stx {
 template<typename Iterator, typename T, typename Compare>
 Iterator LowerBound(Iterator begin, Iterator end,
                     const T& value,
-                    Compare compare)
+                    Compare&& compare)
 {
 	using std::distance;
 	using std::advance;
@@ -25,7 +25,7 @@ Iterator LowerBound(Iterator begin, Iterator end,
 		auto mid = begin;
 		advance(mid, step);
 
-		if (compare(*mid, value)) {
+		if (std::forward<Compare>(compare)(*mid, value)) {
 			begin = mid;
 			advance(begin, 1);
 			dist -= step + 1;
@@ -47,10 +47,10 @@ Iterator LowerBound(Iterator begin, Iterator end, const T& value)
 template<typename Iterator, typename T, typename Compare>
 bool BinarySearch(Iterator begin, Iterator end,
                   const T& value,
-                  Compare compare)
+                  Compare&& compare)
 {
 	auto found = stx::LowerBound(begin, end, value, compare);
-	return found != end && !compare(value, *begin);
+	return found != end && !std::forward<Compare>(compare)(value, *begin);
 }
 
 template<typename Iterator, typename T>
@@ -63,7 +63,7 @@ bool BinarySearch(Iterator begin, Iterator end,
 template<typename Iterator, typename T, typename Compare>
 Iterator UpperBound(Iterator begin, Iterator end,
                     const T& value,
-                    Compare compare)
+                    Compare&& compare)
 {
 	using std::distance;
 	using std::advance;
@@ -78,7 +78,7 @@ Iterator UpperBound(Iterator begin, Iterator end,
 		auto mid = begin;
 		advance(mid, step);
 
-		if (compare(value, *mid)) {
+		if (std::forward<Compare>(compare)(value, *mid)) {
 			/*end = mid;*/
 			dist = step;
 
@@ -101,10 +101,10 @@ Iterator UpperBound(Iterator begin, Iterator end, const T& value)
 template<typename Iterator, typename T, typename Compare>
 std::pair<Iterator, Iterator> EqualRange(Iterator begin, Iterator end,
                                          const T& value,
-                                         Compare compare)
+                                         Compare&& compare)
 {
-	return { LowerBound(begin, end, value, compare),
-	         UpperBound(begin, end, value, compare) };
+	return { LowerBound(begin, end, value, std::forward<Compare>(compare)),
+	         UpperBound(begin, end, value, std::forward<Compare>(compare)) };
 }
 
 template<typename Iterator, typename T>
