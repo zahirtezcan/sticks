7f491609b11b7d51c1a48e102881c4bc9cf82ee8
zahir <zahirtezcan@yahoo.com>
2016-01-01 15:56:20 +0200

    moved equalrange to its own header

--- stx/algorithm/BinarySearch.h
+++ stx/algorithm/BinarySearch.h
@@ -98,22 +98,6 @@ Iterator UpperBound(Iterator begin, Iterator end, const T& value)
 	return stx::UpperBound(begin, end, value, stx::Less());
 }
 
-template<typename Iterator, typename T, typename Compare>
-std::pair<Iterator, Iterator> EqualRange(Iterator begin, Iterator end,
-                                         const T& value,
-                                         Compare&& compare)
-{
-	return { LowerBound(begin, end, value, std::forward<Compare>(compare)),
-	         UpperBound(begin, end, value, std::forward<Compare>(compare)) };
-}
-
-template<typename Iterator, typename T>
-std::pair<Iterator, Iterator> EqualRange(Iterator begin, Iterator end,
-                                         const T& value)
-{
-	return stx::EqualRange(begin, end, value, stx::Less());
-}
-
 }/*end of stx namespace*/
 
 #endif
---
+++ stx/algorithm/EqualRange.h
@@ -0,0 +1,29 @@
+#ifndef STX_ALGORITHM_EQUALRANGE_H
+#define STX_ALGORITHM_EQUALRANGE_H
+
+#include <utility>
+#include <stx/utility/Compare.h>
+#include <stx/algorithm/BinarySearch.h>
+
+namespace stx {
+
+template<typename Iterator, typename T, typename Compare>
+std::pair<Iterator, Iterator> EqualRange(Iterator begin, Iterator end,
+                                         const T& value,
+                                         Compare&& compare)
+{
+	return { LowerBound(begin, end, value, std::forward<Compare>(compare)),
+	         UpperBound(begin, end, value, std::forward<Compare>(compare)) };
+}
+
+template<typename Iterator, typename T>
+std::pair<Iterator, Iterator> EqualRange(Iterator begin, Iterator end,
+                                         const T& value)
+{
+	return stx::EqualRange(begin, end, value, stx::Less());
+}
+
+}/*end of stx namespace*/
+
+#endif
+
